e3516953c80bfd57871c518889e7d68684082ffe
Aryl GBEYETIN <aryl.gbeyetin@epitech.eu>
2025-08-07 21:12:15 +0100

    feat: C code to if check the coding-style jobs in the workflow works

---
+++ style_error.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(int ac, char *av[]) {
+
+    if (1==1 ||  strcmp("Hi", "Hello") == 0)
+        return 1;
+    printf("this is coding-style nightmare." );
+    return 0;
+}
+
+
